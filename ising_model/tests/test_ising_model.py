a2a71de31d302e9b5d5008f5f0cfab8c8a909728
FeiQuantumSoftware <fei8@vt.edu>
2022-03-01 12:14:44 -0500

    install pytest package

--- ising_model/tests/test_ising_model.py
+++ ising_model/tests/test_ising_model.py
@@ -10,6 +10,10 @@ import pytest
 import ising_model
 
 
+def test1():
+    assert(1 == 1)
+
+
 def test_ising_model_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "ising_model" in sys.modules
