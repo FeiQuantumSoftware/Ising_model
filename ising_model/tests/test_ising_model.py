fc062a278824477c518f653dd9b74e209fb2ef8c
FeiQuantumSoftware <fei8@vt.edu>
2022-03-19 14:41:50 -0400

    delete testing test1

--- ising_model/tests/test_ising_model.py
+++ ising_model/tests/test_ising_model.py
@@ -8,10 +8,6 @@ import pytest
 import sys
 
 
-def test1():
-    assert(1 == 1)
-
-
 def test_ising_model_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "ising_model" in sys.modules
